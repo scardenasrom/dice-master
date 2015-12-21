358fa1d685fc55fa153bbce6b7764edba6c49625
scardenas <scardenas@viavansi.com>
2015-12-21 11:39:35 +0100

    Added some screens

--- dice-master/src/main/java/com/herkiusdev/dicemaster/activity/SingleThrowActivity.java
+++ dice-master/src/main/java/com/herkiusdev/dicemaster/activity/SingleThrowActivity.java
@@ -1,10 +1,12 @@
 package com.herkiusdev.dicemaster.activity;
 
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.herkiusdev.dicemaster.R;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.activity_single_throw)
@@ -14,6 +16,11 @@ public class SingleThrowActivity extends AppCompatActivity{
     public void initViews() {
         getSupportActionBar().setTitle("Dice Master - Tirada Única");
         getSupportActionBar().setElevation(0);
+    }
+
+    @Click(R.id.single_throw_d20_btn)
+    public void d20IconClick(){
+        Toast.makeText(SingleThrowActivity.this, "Dice pressed!", Toast.LENGTH_SHORT).show();
     }
 
 }
